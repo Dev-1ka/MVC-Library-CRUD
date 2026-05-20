dbac21937781b2393f5c8b3ffe3407586eaeb9d8
Devika Unnikrishnan <devika.unnikrishnan@fingent.com>
2026-05-20 16:50:26 +0530

    Initial commit

---
+++ CRUD/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Models
+{
+    public class Student
+    {
+        [Key]
+        [Display(Name ="Student ID")]
+        public int StudentId { get; set; }
+        [Required]
+        [Display(Name="Student Name")]
+        public string Name { get; set; }
+        
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Department { get; set; }
+
+        [Required]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+
+    }
+}
